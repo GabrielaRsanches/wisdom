148171ea165e5ebacb2c18640a0e6f0d340dd3a2
Gabriela Sanches <gabi@toki.life>
2023-09-11 15:26:29 -0300

    deleted unnecessary modules

--- wisdom/api/src/app/domain/answer/answer.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { Answer } from './Answer';
-
-@Module({
-  imports: [Answer],
-  controllers: [],
-  providers: [],
-})
-export class AnswerModule {}
--- wisdom/api/src/app/domain/teacher/teacher.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { Teacher } from './Teacher';
-
-@Module({
-  imports: [Teacher],
-  controllers: [],
-  providers: [],
-})
-export class TeacherModule {}
