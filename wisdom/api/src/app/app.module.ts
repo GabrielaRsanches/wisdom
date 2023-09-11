2672969a1c616064ac24f5f2d5d71ad08b07bb67
Gabriela Sanches <gabi@toki.life>
2023-09-11 13:53:41 -0300

    feat: imports from domain

--- wisdom/api/src/app/app.module.ts
+++ wisdom/api/src/app/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Teacher } from './domain/teacher/Teacher';
+import { Student } from './domain/student/Student';
+import { Question } from './domain/question/Question';
+import { Answer } from './domain/answer/Answer';
 
 @Module({
-  imports: [],
+  imports: [
+    Teacher,
+    Student,
+    Question,
+    Answer
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
