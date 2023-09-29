bc7db67dc7db303988ca2ad798d4071ce62b8e35
Gabriela Sanches <gabi@toki.life>
2023-09-29 15:48:27 -0300

    refactor: new web project using Angular CLI and Nebular

--- web/src/server/routes/trpc/[trcp].ts
+++
@@ -1,8 +0,0 @@
-import { appRouter } from '../../trcp/routers';
-import { createContext } from '../../trcp/context';
-import { createTrpcNitroHandler } from '@analogjs/trpc';
-// export API handler
-export default createTrpcNitroHandler({
-  router: appRouter,
-  createContext,
-});
