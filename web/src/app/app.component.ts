3852be25e9b58a5b0759e7d0c48bac4b7cff6b4a
Gabriela Sanches <gabi@toki.life>
2023-09-29 15:50:07 -0300

    feat: web is building and serving

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import HomeComponent from './pages/(home).page';
+import { ChildrenOutletContexts } from '@angular/router';
+
 
 @Component({
   selector: 'web-root',
-  standalone: true,
-  imports: [HomeComponent, RouterOutlet],
-  template: `<router-outlet></router-outlet>`,
+  template: `
+  <nb-layout>
+  <nb-layout-header fixed>Company Name</nb-layout-header>
+
+  <nb-sidebar>Sidebar Content</nb-sidebar>
+
+  <nb-layout-column>
+    Page Content <button nbButton>Hello World</button>
+  </nb-layout-column>
+</nb-layout>
+
+`,
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private contexts: ChildrenOutletContexts) {}
+}
--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,17 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+//import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { RouterModule, Routes } from '@angular/router';
+import { Router, RouterModule, Routes } from '@angular/router';
 import HomeComponent from './pages/(home).page';
 import { TeacherOrStudent } from './pages/teacherOrStudent';
 import StudentRegistry from './pages/studentRegistry';
 import TeacherRegistry from './pages/techerRegistry';
 import { TeachersFeed } from './pages/teachersFeed';
+import { NbButtonModule, NbLayoutModule, NbSidebarModule, NbSidebarService, NbThemeModule } from '@nebular/theme';
 
 
 const routes: Routes = [
-  { path: '/', component: HomeComponent  },
+  { path: 'home', component: AppComponent  },
   { path: 'teacher-or-student', component: TeacherOrStudent },
   { path: 'student-registry', component: StudentRegistry },
   { path: 'teacher-registry', component: TeacherRegistry },
@@ -19,12 +20,22 @@ const routes: Routes = [
 ]
 
 @NgModule({
-  declarations: [ AppComponent, HomeComponent, TeacherOrStudent, StudentRegistry, TeacherRegistry],
-  imports: [ BrowserModule, FormsModule, RouterModule.forRoot(routes)],
-  providers: [],
+  declarations: [ AppComponent, HomeComponent, TeacherOrStudent, StudentRegistry, TeacherRegistry ],
+  imports: [
+    BrowserModule,
+    NbThemeModule.forRoot(),
+    RouterModule.forRoot(routes, { useHash: true }),
+    NbLayoutModule,
+    NbSidebarModule.forRoot(),
+    NbButtonModule // , if this is your app.module
+
+  ],
+  providers: [NbSidebarService],
   bootstrap: [AppComponent],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+
 })
 export class AppModule {
-
+  constructor(router: Router){}
 }
