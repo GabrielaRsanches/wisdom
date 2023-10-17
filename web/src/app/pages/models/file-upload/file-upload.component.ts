51fe560b3e90d391ecc0b677a083967e73c75793
Gabriela Sanches <gabi@toki.life>
2023-10-17 13:56:00 -0300

    deleted styleUrl variable

--- web/src/app/pages/models/file-upload/file-upload.component.ts
+++ web/src/app/pages/models/file-upload/file-upload.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-file-upload',
   templateUrl: './file-upload.component.html',
-  styleUrls: ['./file-upload.component.css']
+ 
 })
 export class FileUploadComponent {
   onFileSelected(event: any) {
