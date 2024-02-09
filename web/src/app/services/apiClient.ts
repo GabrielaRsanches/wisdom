99b18eb6306289484ee2e12710640c896d4ae5d3
Gabriela Sanches <gabi@toki.life>
2024-02-09 08:38:48 -0300

    chore: axios + drizze implementation

--- web/src/app/services/apiClient.ts
+++ web/src/app/services/apiClient.ts
@@ -1,54 +1,59 @@
 import axios from "axios";
 import { CreateTeacherDto } from '../../../../api/dtos/createTeacherDto'
 import { CreateStudentDto } from '../../../../api/dtos/createStudentDto'
-const baseUrl = 'http://localhost:3333/api/';
+import { Injectable } from "@angular/core";
+const baseUrl = 'http://localhost:4200/#/';
 
 const api = axios.create({
   baseURL: baseUrl,
   timeout: 1000,
 });
 
-const wisdomApi = {
-  teacher: {
-    create: async (createTeacher: CreateTeacherDto)=>{
-      try {
-        const created = await api.post('teacher/create', createTeacher)
-        return created
-      } catch (error){
-        console.log(error)
-        return error
-      };
-    },
-    getOne: async (teacherId: string) => {
-      try {
-        const getOne = await api.get(`/teacher/${teacherId}`)
-        console.log(getOne)
-        return getOne
-      } catch(error){
-        console.log(error)
-        return error
+@Injectable()
+export class AxiosService {
+  wisdomApi = {
+    teacher: {
+      create: async (createTeacher: CreateTeacherDto)=>{
+        try {
+          const created = await api.post('teacher/create', createTeacher)
+          return created
+        } catch (error){
+          console.log(error)
+          return error
+        };
+      },
+      getOne: async (teacherId: string) => {
+        try {
+          const getOne = await api.get(`/teacher/${teacherId}`)
+          console.log(getOne)
+          return getOne
+        } catch(error){
+          console.log(error)
+          return error
+        }
       }
-    }
-  },
-  student: {
-    create: async(student: CreateStudentDto)=>{
-      try {
-        const created = await api.post('student/create', student)
-        return created
-      } catch (error){
-        console.log(error)
-        return error
-      };
     },
-    getOne: async (studentId: string) => {
-      try {
-        const getOne = await api.get(`/teacher/${studentId}`)
-        console.log(getOne)
-        return getOne
-      } catch(error){
-        console.log(error)
-        return error
+    student: {
+      create: async(student: CreateStudentDto)=>{
+        try {
+          const created = await api.post('student/create', student)
+          return created
+        } catch (error){
+          console.log(error)
+          return error
+        };
+      },
+      getOne: async (studentId: string) => {
+        try {
+          const getOne = await api.get(`/teacher/${studentId}`)
+          console.log(getOne)
+          return getOne
+        } catch(error){
+          console.log(error)
+          return error
+        }
       }
     }
   }
 }
+
